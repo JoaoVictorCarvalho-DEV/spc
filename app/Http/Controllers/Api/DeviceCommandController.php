6733235e706462a05d880a29cc6265b524fb21cd
João Víctor Miranda Carvalho <joao002miranda@gmail.com>
2026-04-20 10:36:16 -0300

    feat:adicionando rota de listar e criar comandos

--- app/Http/Controllers/Api/DeviceCommandController.php
+++ app/Http/Controllers/Api/DeviceCommandController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Device;
+use App\Models\DeviceCommand;
 use Illuminate\Http\Request;
 
 class DeviceCommandController extends Controller
 {
-    //
+
+    public function next(Request $request)
+    {
+        $device = $request->input('device');
+
+        $deviceCommand = $device->deviceCommands()
+            ->where('executed', false)
+            ->oldest()
+            ->first();
+
+
+        return response()->json(
+            [
+                'device_command' => $deviceCommand,
+            ],
+            200
+        );
+    }
+
+    public function store(Request $request)
+    {
+        $device = $request->input('device');
+
+        $deviceCommand = DeviceCommand::create([
+            'command' => 'turn_on',
+            'device_id' => $device->id
+        ]);
+
+        if (!$deviceCommand) {
+            return response()->json(['message' => 'error'], 400);
+        }
+
+        return response()->json(['message' => 'success'], 201);
+    }
 }
